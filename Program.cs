d5407d7f1df0ad86d7dc3d70928d78e027382463
Dmitriy Valeev <ValeevDmitriy@gmail.com>
2021-06-01 11:12:29 +1000

    init commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LearningCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t = Task.CompletedTask;
+            int sec = 0;
+
+            ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
+
+            while(true)
+            {
+                if(Console.KeyAvailable)
+                {
+                    keyInfo = Console.ReadKey(true);
+                    Console.WriteLine($"Key: {keyInfo.Key}");
+                }
+
+                if(!t.IsCompleted)
+                    continue;
+
+                Console.WriteLine($"Second: {sec++}");
+                t = DelayGame();
+            }
+        }
+
+        static async Task DelayGame()
+        {
+            await Task.Delay(1000);
+        }
+    }
+
+
+}
